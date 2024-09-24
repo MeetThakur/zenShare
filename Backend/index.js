d39dd9f553470fbc9c1ea94fc258b71bdcbb4e30
Meet Thakur <meet11india@gmail.com>
2024-09-24 23:21:39 +0530

    modified sever

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,27 @@
-const express = require('express');
-const app = express(); 
-const cors = require('cors');
-const fs = require('fs');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const fs = require("fs");
 app.use(cors());
 
-let text = fs.readFileSync('text.txt', 'utf8');
+let text = fs.readFileSync("data.txt", "utf8");
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.get('/getText', (req, res) => {
-    res.send(text); 
+app.get("/getText", (req, res) => {
+  res.send(text);
 });
 
-app.post('/setText', (req, res) => {
-    console.log((req.body)[0].text);
-    fs.writeFileSync('text.txt', (req.body)[0].text);
-    res.send('Text has been updated');
+app.post("/setText", (req, res) => {
+  console.log(req.body[0].text);
+  fs.writeFileSync("data.txt", req.body[0].text);
+  res.send("Text has been updated");
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
-
-
